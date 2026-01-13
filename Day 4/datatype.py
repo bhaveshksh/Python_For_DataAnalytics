17111c2b915d8f691c04912ddd7ec74d3066747a
bhaveshksh <bhaveshkshirsagar50@gmail.com>
2026-01-13 19:36:17 +0530

    Adding Files

---
+++ Day 4/datatype.py
@@ -0,0 +1,8 @@
+a = 3
+print(f"a = {a} is data type is {type(a)}")
+
+b= 4.5
+print(f"b = {b} is data type is {type(b)}")
+
+c= 5+4j
+print(f"c = {c} is data type is {type(c)}")
---
+++ Day 4/formatedstring.py
@@ -0,0 +1,11 @@
+a = 2
+b = 4
+print('a= ',a,'b=',b, 'a+b=',a+b)
+
+print(f"a = {a} and b= {b} op a+b = {a+b}")
+
+oi = f'''
+line 1 value of a is {a}
+line 2 valueb of b is {b}
+line 3 addition of a+b is {a+b}'''
+print(oi)
